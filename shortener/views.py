d4fca917d6134b198445d1cbf392e267fbb58b92
Vitaly Chernyshov <81708357+vitaliy-chernyshov@users.noreply.github.com>
2022-04-17 00:28:26 +0300

    view для главной

--- shortener/views.py
+++ shortener/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from shortener.models import LongShortUrl
+
 
 def index(request):
     template = 'index.html'
@@ -6,3 +11,10 @@ def index(request):
         'text': 'hello world'
     }
     return render(request, template, context)
+
+
+class IndexView(CreateView):
+    template_name = 'index.html'
+    model = LongShortUrl
+    fields = ('long', 'short', 'is_public')
+    success_url = reverse_lazy('shortener:index')
