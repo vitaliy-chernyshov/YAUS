d13582726434e49339c1e982a35977b15105ae4f
Vitaly Chernyshov <81708357+vitaliy-chernyshov@users.noreply.github.com>
2022-04-26 16:57:09 +0300

    страница с личными ссылками

--- shortener/urls.py
+++ shortener/urls.py
@@ -6,5 +6,7 @@ app_name = 'shortener'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('me/', views.ProfileView.as_view(), name='profile'),
     path('<str:url>/', views.CustomRedirectView.as_view(), name='redirect'),
+
 ]
